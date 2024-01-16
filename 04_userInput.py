de6f7d3e0867cf1769f839f2db7dc89248b20bb1
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-01-16 23:16:55 +0530

    User input

---
+++ 04_userInput.py
@@ -0,0 +1,39 @@
+# name = input("Enter yoir name: ")
+# age = int(input("Enter your age: "))
+
+# age = age+2
+
+# print(f"hello {name}")
+# print(f"Your age after 2 years {age}")
+
+
+# 1. mad libs
+# adjective1 = input("enter an adjective: ")
+# noun = input("Enter a noun: ")
+# adjective2 = input("enter an adjective: ")
+# verb = input("enter a verb: ")
+# adjective3 = input("enter an adjective: ")
+
+# print(f"Today I went to a {adjective1} zoo")
+# print(f"in a exhibit, I saw {noun}")
+# print(f"{noun} was {adjective2} and {verb}ing")
+# print(f"i was {adjective3}")
+
+
+# 2. area calc
+# length = int(input("lenght: "))
+# width = int(input("width: "))
+# height = float(input("height: "))
+# area = length*width*height
+
+# print(f"Area: {area}")
+
+
+# 3. Shopping cart 
+item = input("item name: ")
+price = float(input("price: "))
+quantity = int(input("qua: "))
+
+total = price*quantity
+
+print(f"total cost is {round(total, 2)}")
