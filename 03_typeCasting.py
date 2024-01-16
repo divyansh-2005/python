fba53309c11e370f8f12def38f915ce7d5c8aaad
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-01-16 22:59:40 +0530

    Type casting

---
+++ 03_typeCasting.py
@@ -0,0 +1,37 @@
+# typecasting
+name = "Bro"
+age = 18
+gpa = 1.9
+student = True
+
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+# print(type(student))
+
+# explict cast
+
+age = float(age)
+# print(age)
+# print(type(age))
+
+gpa = int(gpa)
+# print(gpa)
+# print(type(gpa))
+
+student = str(student)
+# print(student)
+# print(type(student))
+
+age = bool(age)
+# print(age)
+# print(type(age))
+
+
+#implicit cast
+
+x=2
+y=2.0
+
+x = x / y
+print(x)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,2 @@
-online = True
-# for_sale = False
-# running = True
-
-# print(f"Are u online? : {online}")
-
-# if running:
-#     print("The game is running")
-# else:
-#     print("Game over")
+print(age)
+print(type(age))
