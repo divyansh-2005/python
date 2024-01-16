f4dce86848160119e63b71fd66b13cb1f3ba5f06
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-01-16 23:37:31 +0530

    Math module

---
+++ 05_math.py
@@ -0,0 +1,37 @@
+import math
+
+x = 3.14
+y = 4
+z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4,3)
+# result = max(x,y,z)
+# result = min(x,y,z)
+
+# print(result)
+
+# print(math.pi)
+# print(math.e)
+
+x=9.9
+# result = math.sqrt(9)
+# result = math.ceil(x)
+# result = math.floor(x)
+# print(result)
+
+
+# radius = float(input("enter radius: "))
+# c = 2*math.pi*radius
+# print(round(c,2))
+
+# radius = float(input("enter radius: "))
+# area = math.pi* pow(radius,2)
+# print(round(area,3))
+
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+
+c = math.sqrt(pow(a,2) + pow(b,2))
+print(c)
