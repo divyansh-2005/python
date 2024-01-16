e644785314266d891b365e17af7f8b7c85e7804a
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-01-16 22:44:24 +0530

    Python variables and data types

---
+++ 02_var_dataType.py
@@ -0,0 +1,54 @@
+# variable
+
+age = 21
+# print("you are " + str(age) + ' year old')
+# print("you are", age, "years old")
+# print(f"you are {age} years old")
+
+x, y ,z = 1,2,3
+x=y=z=1
+# print(x)
+# print(y)
+# print(z)
+
+#Data Types
+# 1.INTEGER
+
+age = 21
+players = 2
+quantity = 5
+# print(f"you are {age} years old")
+# print(f"there are {players} players online")
+
+# 2.FLOAT
+
+gpa = 3.2
+distance = 2.5
+price = 10.99
+# print(f"Your gpa is {gpa}")
+# print(f"You ran {distance}KM")
+# print(f"The price is ${price}")
+
+# 3.STRING
+
+name = "Bro"
+food = "dosa"
+email = "Divya@gmail.com"
+
+# print(f"Hello {name}")
+# print(f"I like {food}")
+# print(f"Your email is: {email}")
+
+# 4.BOOLEAN
+
+online = True
+for_sale = False
+running = True
+
+# print(f"Are u online? : {online}")
+
+# if running:
+#     print("The game is running")
+# else:
+#     print("Game over")
+    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+online = True
+# for_sale = False
+# running = True
+
+# print(f"Are u online? : {online}")
+
+# if running:
+#     print("The game is running")
+# else:
+#     print("Game over")
