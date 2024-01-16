8e08e52766df8893550ec8150b565ebca9c0cb37
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-01-16 23:53:22 +0530

    If statements

---
+++ 06_if_else.py
@@ -0,0 +1,29 @@
+# age = int(input("Enter your age: "))
+
+# if age >= 100:
+#     print("too old")
+# elif age >= 18:
+#     print("yes")
+# elif age <= 0:
+#     print("not born")
+# else:
+#     print("no")
+
+# response = input("yes/no: ")
+# if response == "yes":
+#     print("Have some food")
+# else:
+#     print("NO food")
+
+# name = input("Enter your name: ")
+
+# if name == "":
+#     print("you did not enter your name")
+# else:
+#     print(f"Hello {name}")
+
+for_sale = True
+if for_sale:
+    print("item for sale")
+else:
+    print("item Not for sale")
