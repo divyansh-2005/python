157998a15e32d3becc13d70869bcf69505f3194a
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-01-17 07:59:50 +0530

    Logical operators in Python

---
+++ 10_Logical_operators.py
@@ -0,0 +1,19 @@
+# and , or , not
+
+temp = -10
+sunny = True
+
+# if temp > 0 and temp<30:
+#     print("Good")
+# else:
+#     print("Bad")
+
+# if temp >= 0 or temp >= 30:
+#     print("Good")
+# else:
+#     print("Bad")
+
+if not sunny:
+    print("It is sunny outside")
+else:
+    print("It is cloudy outside")
